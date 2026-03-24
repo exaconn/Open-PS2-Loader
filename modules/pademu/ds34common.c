f9717f7676a6d7db588a1c042c3eaff36ec414b0
exaconn <113335383+exaconn@users.noreply.github.com>
2026-03-24 11:40:57 +0100

    Implement anti-deadzone for stick and fix pressure mappings
    Added anti-deadzone processing for stick input and corrected pressure button mappings.

--- modules/pademu/ds34common.c
+++ modules/pademu/ds34common.c
@@ -41,14 +41,33 @@ void translate_pad_guitar(const struct ds3guitarreport *in, struct ds2report *ou
     }
 }
 
+// Anti-deadzone here
+uint8_t process_stick(uint8_t in)
+{
+    int adz = 38;
+    int out = (int)in;
+   
+    if (out >= 128) {
+        out += adz;
+    } else {
+        out -= adz;
+    }
+   
+    if (out < 0) out = 0;
+    if (out > 255) out = 255;
+
+    return (uint8_t)out;
+}
+    
 void translate_pad_ds3(const struct ds3report *in, struct ds2report *out, u8 pressure_emu)
 {
     out->nButtonStateL = ~in->ButtonStateL;
-    out->nButtonStateH = ~in->ButtonStateH;
+    out->nButtonStateH = ~(in->L1 | in->R1 << 1 | in->L2 << 2 | in->R2 << 3 | in->Triangle << 4 | in->Circle << 5 | in->Cross << 6 | in->Square << 7);
 
     out->RightStickX = in->RightStickX;
     out->RightStickY = in->RightStickY;
-    out->LeftStickX = in->LeftStickX;
+    // Anti-deadzone here
+    out->LeftStickX = in->LeftStickX;//process_stick(in->LeftStickX);
     out->LeftStickY = in->LeftStickY;
 
     if (pressure_emu) { // needs emulating pressure buttons
@@ -62,10 +81,10 @@ void translate_pad_ds3(const struct ds3report *in, struct ds2report *out, u8 pre
         out->PressureCross = in->Cross * 255;
         out->PressureSquare = in->Square * 255;
 
-        out->PressureL1 = in->L1 * 255;
-        out->PressureR1 = in->R1 * 255;
-        out->PressureL2 = in->L2 * 255;
-        out->PressureR2 = in->R2 * 255;
+        out->PressureL1 = in->L2 * 255;
+        out->PressureR1 = in->R2 * 255;
+        out->PressureL2 = in->L1 * 255;
+        out->PressureR2 = in->R1 * 255;
     } else {
         out->PressureRight = in->PressureRight;
         out->PressureLeft = in->PressureLeft;
@@ -77,10 +96,10 @@ void translate_pad_ds3(const struct ds3report *in, struct ds2report *out, u8 pre
         out->PressureCross = in->PressureCross;
         out->PressureSquare = in->PressureSquare;
 
-        out->PressureL1 = in->PressureL1;
-        out->PressureR1 = in->PressureR1;
-        out->PressureL2 = in->PressureL2;
-        out->PressureR2 = in->PressureR2;
+        out->PressureL1 = in->PressureL2;
+        out->PressureR1 = in->PressureR2;
+        out->PressureL2 = in->PressureL1;
+        out->PressureR2 = in->PressureR1;
     }
 }
 
@@ -141,4 +160,143 @@ void translate_pad_ds4(const struct ds4report *in, struct ds2report *out, u8 hav
     out->PressureR1 = in->R1 * 255;
     out->PressureL2 = in->PressureL2;
     out->PressureR2 = in->PressureR2;
+}
+
+void translate_wheel_df(const struct dfreport *in, struct ds2report *out)
+{
+    /*
+    static const u8 dpad_mapping[] = {
+        (DS2ButtonUp),
+        (DS2ButtonUp | DS2ButtonRight),
+        (DS2ButtonRight),
+        (DS2ButtonDown | DS2ButtonRight),
+        (DS2ButtonDown),
+        (DS2ButtonDown | DS2ButtonLeft),
+        (DS2ButtonLeft),
+        (DS2ButtonUp | DS2ButtonLeft),
+        0,
+    };
+
+    out->nButtonStateL = ~(in->select | in->L3 << 1 | in->R3 << 2 | in->start << 3 | dpad_mapping[in->hat]);
+    */
+    
+    u8 up = 0, down = 0, left = 0, right = 0;
+
+    switch (in->hat) {
+        case 0:
+            up = 1;
+            break;
+        case 1:
+            up = 1;
+            right = 1;
+            break;
+        case 2:
+            right = 1;
+            break;
+        case 3:
+            down = 1;
+            right = 1;
+            break;
+        case 4:
+            down = 1;
+            break;
+        case 5:
+            down = 1;
+            left = 1;
+            break;
+        case 6:
+            left = 1;
+            break;
+        case 7:
+            up = 1;
+            left = 1;
+            break;
+        case 8:
+            up = 0;
+            down = 0;
+            left = 0;
+            right = 0;
+            break;
+    }
+            
+    out->nButtonStateL = ~(in->select | in->L3 << 1 | in->R3 << 2 | in->start << 3 | up << 4 | right << 5 | down << 6 | left << 7);            
+    out->nButtonStateH = ~(in->L2 | in->R2 << 1 | in->L1 << 2 | in->R1 << 3 | in->triangle << 4 | in->circle << 5 | in->cross << 6 | in->square << 7);
+
+    out->RightStickX = 0x7F; // neutral
+    out->RightStickY = 0x7F; // neutral
+    out->LeftStickX = process_stick(in->wheel >> 2); // in->wheel >> 2; // 8bit conversion
+    out->LeftStickY = 0x7F; // neutral
+    
+    out->PressureRight = right * 255;
+    out->PressureLeft = left * 255;
+    out->PressureUp = up * 255;
+    out->PressureDown = down * 255;
+
+    /*
+    out->PressureRight = out->nRight ? 0 : 255;
+    out->PressureLeft = out->nLeft ? 0 : 255;
+    out->PressureUp = out->nUp ? 0 : 255;
+    out->PressureDown = out->nDown ? 0 : 255;
+    */
+
+    out->PressureTriangle = in->triangle * 255;
+    out->PressureCircle = in->circle * 255;
+    out->PressureCross = 255 - in->gasPedal; // inverted
+    out->PressureSquare = 255 - in->brakePedal; // inverted
+
+    out->PressureL1 = in->L1 * 255;
+    out->PressureR1 = in->R1 * 255;
+    out->PressureL2 = in->L2 * 255;
+    out->PressureR2 = in->R2 * 255;
+}
+
+void translate_wheel_negcon(const struct dfreport *in, struct ncreport *out)
+{
+    uint8_t up = 0, down = 0, left = 0, right = 0;
+
+    switch (in->hat) {
+        case 0:
+            up = 1;
+            break;
+        case 1:
+            up = 1;
+            right = 1;
+            break;
+        case 2:
+            right = 1;
+            break;
+        case 3:
+            down = 1;
+            right = 1;
+            break;
+        case 4:
+            down = 1;
+            break;
+        case 5:
+            down = 1;
+            left = 1;
+            break;
+        case 6:
+            left = 1;
+            break;
+        case 7:
+            up = 1;
+            left = 1;
+            break;
+        case 8:
+            up = 0;
+            down = 0;
+            left = 0;
+            right = 0;
+            break;
+            
+    }
+            
+    out->nButtonStateL = ~(in->start << 3 | up << 4 | right << 5 | down << 6 | left << 7);
+    out->nButtonStateH = ~(in->R1 << 3 | in->triangle << 4 | in->circle << 5);
+               
+    out->SteeringAxis = in->wheel >> 2; // 8bit conversion
+    out->AnalogI = 255 - in->gasPedal; // inverted
+    out->AnalogII = 255 - in->brakePedal; // inverted
+    out->AnalogL = in->L1 * 255;
 }
